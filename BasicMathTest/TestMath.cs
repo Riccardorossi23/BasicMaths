66c8df4b60eaaaa6633aa90f2bcb71fcbf53852b
Riccardorossi23 <55913260+Riccardorossi23@users.noreply.github.com>
2019-11-14 17:01:49 +0100

    Add project files.

---
+++ BasicMathTest/TestMath.cs
@@ -0,0 +1,39 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using BasicMaths;
+
+namespace BasicMathTest
+{
+    [TestClass]
+    public class TestMath
+    {
+        [TestMethod]
+        public void TestAdd()
+        {
+            BasicMath bm = new BasicMath();
+            double res = bm.Add(10, 10);
+            Assert.AreEqual(res, 20);
+        }
+        [TestMethod]
+        public void Test_SubstractMethod()
+        {
+            BasicMath bm = new BasicMath();
+            double res = bm.Substract(10, 10);
+            Assert.AreEqual(res, 0);
+        }
+        [TestMethod]
+        public void Test_DivideMethod()
+        {
+            BasicMath bm = new BasicMath();
+            double res = bm.divide(10, 5);
+            Assert.AreEqual(res, 2);
+        }
+        [TestMethod]
+        public void Test_MultiplyMethod()
+        {
+            BasicMath bm = new BasicMath();
+            double res = bm.Multiply(10, 10);
+            Assert.AreEqual(res, 100);
+        }
+    }
+}
