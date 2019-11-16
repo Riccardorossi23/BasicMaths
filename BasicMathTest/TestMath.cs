3d9a8f2a0130eacdb7713b76ac8d97d807bb36db
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2019-11-16 12:07:34 +0100

    eseUnitTestMaths

--- BasicMathTest/TestMath.cs
+++ BasicMathTest/TestMath.cs
@@ -8,28 +8,28 @@ namespace BasicMathTest
     public class TestMath
     {
         [TestMethod]
-        public void TestAdd()
+        public  static TestAdd()
         {
             BasicMath bm = new BasicMath();
             double res = bm.Add(10, 10);
             Assert.AreEqual(res, 20);
         }
         [TestMethod]
-        public void Test_SubstractMethod()
+        public static Test_SubstractMethod()
         {
             BasicMath bm = new BasicMath();
             double res = bm.Substract(10, 10);
             Assert.AreEqual(res, 0);
         }
         [TestMethod]
-        public void Test_DivideMethod()
+        public  static Test_DivideMethod()
         {
             BasicMath bm = new BasicMath();
             double res = bm.divide(10, 5);
             Assert.AreEqual(res, 2);
         }
         [TestMethod]
-        public void Test_MultiplyMethod()
+        public static Test_MultiplyMethod()
         {
             BasicMath bm = new BasicMath();
             double res = bm.Multiply(10, 10);
